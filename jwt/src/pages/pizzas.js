1af6d108fac730366b2ee4814c7c12604a13b179
Doris Andrea Valverde Jara <andrea.avj03@gmail.com>
2025-05-15 23:34:36 -0400

    agregando imagenes

--- jwt/src/pages/pizzas.js
+++ jwt/src/pages/pizzas.js
@@ -1,58 +1,55 @@
+import pestoMargarita from '../assets/img/marg.jpg';
+import criolla from '../assets/img/criolla.png';
+import pepperoni from '../assets/img/pepperoni.png';
+import todasLasCarnes from '../assets/img/todas-las-carnes.png';
+import clasic from '../assets/img/classic.png';
+
 const pizzas = [
   {
-      id: 1,
-      name: "pesto-margarita",
-      ingredients: ["Tomate cherry", "Mozzarella", "Pesto"],
-      stock: 8,
-      quantity: 0,
-      price: 11.990,
-      image: "../img/holy-pesto-margarita.PNG",
+    id: "p001",
+    name: "Pesto Margarita",
+    ingredients: ["Tomate cherry", "Mozzarella", "Pesto"],
+    price: 11990,
+    image: pestoMargarita,
+    description: "Una pizza fresca con pesto casero y mozzarella.",
+    quantity: 0
   },
   {
-      id: 2,
-      name: "criolla",
-      ingredients: ["Tomate", "Chorizo", "Pimiento verde"],
-      stock: 10,
-      quantity: 0,
-      price: 12.990,
-      image: "../img/criolla.PNG",
+    id: "p002",
+    name: "Criolla",
+    ingredients: ["Tomate", "Chorizo", "Pimiento verde"],
+    price: 12990,
+    image: criolla,
+    description: "Una pizza intensa y sabrosa al estilo criollo.",
+    quantity: 0
   },
   {
-      id: 3,
-      name: "Pepperoni",
-      ingredients: ["Tomate", "Pepperoni", "Parmesano"],
-      stock: 13,
-      quantity: 0,
-      price: 13.990,
-      image: "../img/pepperoni.PNG",
+    id: "p003",
+    name: "Pepperoni",
+    ingredients: ["Tomate", "Pepperoni", "Parmesano"],
+    price: 13990,
+    image: pepperoni,
+    description: "Una clásica pizza con abundante pepperoni.",
+    quantity: 0
   },
   {
-      id: 4,
-      name: "Pepperoni",
-      ingredients: ["Tomate", "Pepperoni", "Parmesano"],
-      stock: 13,
-      quantity: 0,
-      price: 13.990,
-      image: "../img/pepperoni.PNG",
+    id: "p004",
+    name: "Todas las Carnes",
+    ingredients: ["Jamón", "Pepperoni", "Tocino"],
+    price: 13990,
+    image: todasLasCarnes,
+    description: "Una pizza para los amantes de la carne.",
+    quantity: 0
   },
   {
-      id: 5,
-      name: "TODAS LAS CARNES",
-      ingredients: ["Jamón", "Pepperoni", "Tocino"],
-      stock: 6,
-      quantity: 0,
-      price: 13.990,
-      image: "../img/todas-las-carnes.PNG",
-  },
-  {
-      id: 6,
-      name: "Clasic",
-      ingredients: ["Aceitunas", "Jamón", "Mozzarella"],
-      stock: 8,
-      quantity: 0,
-      price: 10.990,
-      image: "../img/classic.PNG",
-  },
+    id: "p005",
+    name: "Clasic",
+    ingredients: ["Aceitunas", "Jamón", "Mozzarella"],
+    price: 10990,
+    image: clasic,
+    description: "Una combinación clásica que nunca falla.",
+    quantity: 0
+  }
 ];
 
 export default pizzas;
